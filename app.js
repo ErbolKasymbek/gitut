7218ca9afb1ef66ab3e6e8be789192309148760e
Erbol Kasymbek <erbafredev@gmail.com>
2024-01-18 13:26:07 +0600

    added new planets

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const planets = ["Earth", "Mars", "Jupiter"];
+const planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptun",
+];
 
 function showArrayItems(arr) {
   arr.forEach((el) => {
