c658fd3962f5a142b4c43cb64160135f25017462
Erbol Kasymbek <erbafredev@gmail.com>
2024-01-18 16:10:50 +0600

    modified

--- string.js
+++ string.js
@@ -1,13 +1,3 @@
 const str = "North";
 const nbsp = " ";
 const str2 = "America";
-
-console.log(str.concat(nbsp, str2));
-
-function conStr(a, b) {
-  return a + b;
-}
-
-conStr(str, str2);
-
-console.log(conStr(str, str2));
