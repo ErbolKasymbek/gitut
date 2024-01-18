3f9ff7c2e5a4b16b30f68c8ef7e6006c816d2a39
Erbol Kasymbek <erbafredev@gmail.com>
2024-01-18 15:33:31 +0600

    added new function version

--- string.js
+++ string.js
@@ -3,3 +3,11 @@ const nbsp = " ";
 const str2 = "America";
 
 console.log(str.concat(nbsp, str2));
+
+function conStr(a, b) {
+  return a + b;
+}
+
+conStr(str, str2);
+
+console.log(conStr(str, str2));
